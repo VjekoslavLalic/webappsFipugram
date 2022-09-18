453b33e959467b75fc3163ae116c4cb5ad24d461
VjekoslavLalic <72546167+VjekoslavLalic@users.noreply.github.com>
2022-09-18 23:17:06 +0200

    Komponenta InstagramCard

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav id="nav" class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="#">
+      <img src="@/assets/fipugram.png" alt="Logo" height="40" class="d-inline-block align-top">
+    </a>
+    <button 
+    class="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbarToggler"
+    aria-controls="navbarToggler"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+    > 
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse" id="navbarToggler">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">Login</router-link>
+         </li>
+          <li class="nav-item"> 
+            <router-link to="/signup" class="nav-link">Signup</router-link></li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Pretraga" aria-label="search">
+      </form>
     </div>
+</nav>
     <router-view/>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-7">
+      <instagram-card />
+    </div>
+    <div class="col-3">... ovdje ide desni stupac!</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import InstagramCard from "@/components/InstagramCard.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    InstagramCard,
+  },
+};
 </script>
