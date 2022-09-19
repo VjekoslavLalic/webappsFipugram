17ff2563944e9f1e5aed48ec5ddae09bc5fd6f76
VjekoslavLalic <72546167+VjekoslavLalic@users.noreply.github.com>
2022-09-19 21:15:24 +0200

    FilteredCards zajedno sa instagramcard komponentom

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,64 @@
   <div id="app">
     <nav id="nav" class="navbar navbar-expand-lg navbar-light">
       <a class="navbar-brand" href="#">
-      <img src="@/assets/fipugram.png" alt="Logo" height="40" class="d-inline-block align-top">
-    </a>
-    <button 
-    class="navbar-toggler"
-    type="button"
-    data-toggle="collapse"
-    data-target="#navbarToggler"
-    aria-controls="navbarToggler"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-    > 
-    <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="navbar-collapse" id="navbarToggler">
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <img
+          src="@/assets/fipugram.png"
+          alt="Logo"
+          height="40"
+          class="d-inline-block align-top"
+        />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarToggler"
+        aria-controls="navbarToggler"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item">
             <router-link to="/" class="nav-link">Home</router-link>
           </li>
           <li class="nav-item">
             <router-link to="/login" class="nav-link">Login</router-link>
-         </li>
-          <li class="nav-item"> 
-            <router-link to="/signup" class="nav-link">Signup</router-link></li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Pretraga" aria-label="search">
-      </form>
+          </li>
+          <li class="nav-item">
+            <router-link to="/signup" class="nav-link">Signup</router-link>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            v-model="store.searchTerm"
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Pretraga"
+            aria-label="search"
+          />
+        </form>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view />
     </div>
-</nav>
-    <router-view/>
   </div>
 </template>
-
+<script>
+import store from "@/store";
+export default {
+  name: "app",
+  data() {
+    return {
+      store: store,
+    };
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,39 @@
   <div class="row">
     <div class="col-2"></div>
     <div class="col-7">
-      <instagram-card v-for="card in cards" :key="card" :info="card" />
+      <instagram-card
+        v-for="card in filteredCards"
+        :key="card.url"
+        :info="card"
+      />
     </div>
     <div class="col-3"></div>
   </div>
 </template>
 
 <script>
+// <instagram-card v-for="(card, drac) in filteredCards" :key="drac" :info="card" /> moje rjesenje
 // @ is an alias to /src
 import InstagramCard from "@/components/InstagramCard.vue";
+import store from "@/store";
 let cards;
 
 cards = [
-  "https://picsum.photos/id/1/400/400",
-  "https://picsum.photos/id/2/400/400",
-  "https://picsum.photos/id/3/400/400",
+  {
+    url: "https://picsum.photos/id/1/400/400",
+    description: "laptop",
+    time: "few minutes ago",
+  },
+  {
+    url: "https://picsum.photos/id/2/400/400",
+    description: "laptop 2",
+    time: "few minutes ago",
+  },
+  {
+    url: "https://picsum.photos/id/3/400/400",
+    description: "laptop 3",
+    time: "few minutes ago",
+  },
 ];
 
 export default {
@@ -24,7 +42,14 @@ export default {
   data: function () {
     return {
       cards: cards,
+      store: store,
     };
+  },
+  computed: {
+    filteredCards() {
+      let termin = this.store.searchTerm;
+      return this.cards.filter((card) => card.description.indexOf(termin) >= 0);
+    },
   },
   components: {
     InstagramCard,
