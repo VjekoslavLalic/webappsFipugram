d7f71419dc508091aabb3ec41d9124292925dc45
VjekoslavLalic <72546167+VjekoslavLalic@users.noreply.github.com>
2022-09-19 22:40:53 +0200

    Firebase import file

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+//import { initializeApp } from "firebase/app";
+//import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+//const db = getFirestore(app);
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import "firebase/compat/database";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsUdYvnBIdB0JmzFzNki4jDVeOgtw_yNg",
+  authDomain: "fipugram-1b08c.firebaseapp.com",
+  projectId: "fipugram-1b08c",
+  storageBucket: "fipugram-1b08c.appspot.com",
+  messagingSenderId: "312614484015",
+  appId: "1:312614484015:web:c24dddb6c57a17d05caeda"
+};
+
+//const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+export {
+    firebase,  // kljuc firebase vrijednost firebase
+}
